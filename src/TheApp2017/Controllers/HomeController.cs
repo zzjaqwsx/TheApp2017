e8999d9659cad3ca581722bad2f8201571035ca0
MSI-FREEMAN <z4168273968@gmail.com>
2017-09-16 15:42:23 -0400

    eff

--- src/TheApp2017/Controllers/HomeController.cs
+++ src/TheApp2017/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using TheApp2017.Model;
 
 namespace TheApp2017.Controllers
 {
@@ -25,6 +29,52 @@ namespace TheApp2017.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public async void SubmitMessage(SubmitMessageModel formData)
+        {
+            bool success = true;
+            try
+            {
+                await SendEmailAsync(formData.Email, formData.Subject, formData.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                success = false;
+            }
+
+
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("message", email));
+            emailMessage.To.Add(new MailboxAddress("", "freeman.zhou039@gmail.com"));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+
+                client.Connect("smtp.gmail.com", 587, false);
+                // Note: only needed if the SMTP server requires authentication 
+                // Error 5.5.1 Authentication  
+                //client.Authenticate("freeman.zhou039@gmail.com", "zzj030309");
+                client.Send(emailMessage);
+                Console.WriteLine("The mail has been sent successfully !!");
+                Console.ReadLine();
+                client.Disconnect(true);
+
+
+                //client.LocalDomain = "smtp.gmail.com";
+                //await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.None).ConfigureAwait(false);
+                //await client.SendAsync(emailMessage).ConfigureAwait(false);
+                //await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
         }
 
         public IActionResult Error()
---
+++ src/TheApp2017/Model/SubmitMessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheApp2017.Model
+{
+    public class SubmitMessageModel
+    {
+        public String Name {get;set;}
+        public String Subject { get; set; }
+        public String Email { get; set; }
+        public int Phone { get; set; }
+        public String Message { get; set; }
+
+    }
+}
